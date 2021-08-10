05a7b18ff86a3b99c57b2b963e22cf919839ca55
Jadurani Davalos <jadurani.davalos@gmail.com>
2021-08-11 00:40:11 +0800

    show data points

--- src/app/map.service.ts
+++ src/app/map.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 import { environment } from "../environments/environment";
+import SENSOR_DATA from '../assets/sensors.json';
+import { fromEvent } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -24,6 +26,39 @@ export class MapService {
       center: [this.lng, this.lat]
     })
    this.map.addControl(new mapboxgl.NavigationControl());
+
+   fromEvent(this.map, 'load')
+    .subscribe(() => {
+      this.showDataPoints();
+    })
+  }
+
+  showDataPoints() {
+    const _this = this;
+    SENSOR_DATA .features.forEach(function (marker) {
+      // create a HTML element for each feature
+      // const el = document.createElement('div');
+      // el.className = 'marker';
+
+      // make a marker for each feature and add to the map
+      // new mapboxgl.Marker()
+      //   .setLngLat(marker.geometry.coordinates as [number, number])
+      //   .addTo(_this.map);
+      // console.log(marker);
+      new mapboxgl.Marker()
+        .setLngLat(marker.geometry.coordinates as [number, number])
+        .setPopup(
+          new mapboxgl.Popup().setHTML(
+            `
+          <div>
+            <div>#${marker.properties.station_id} - ${marker.properties.location}</div>
+            <small>Rain Value Sum: ${marker.properties.rain_value_sum}</small>
+          </div>
+        `
+          )
+        )
+        .addTo(_this.map);
+    });
   }
 
 }
