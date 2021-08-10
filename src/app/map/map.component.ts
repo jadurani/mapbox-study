05c0aa4df2f506e2895997361fbdee19c8cdf2f3
Jadurani Davalos <jadurani.davalos@gmail.com>
2021-08-11 00:22:40 +0800

    added mapservice

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MapService } from '../map.service';
 
 @Component({
   selector: 'app-map',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  constructor(private map: MapService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.map.buildMap()
   }
 
 }
